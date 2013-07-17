18ae00ddaa7791eb28886323fa732f2da5c5a241
Jon Vlachogiannis <darksun4@gmail.com>
2013-07-18 01:17:07 +0300

    packaging into a proper release

---
+++ src/storage_server.erl
@@ -0,0 +1,36 @@
+-module(storage_server).
+
+-behaviour(gen_server).
+
+-export([start_link/0, import_data/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+init([]) ->
+    {ok, []}.
+
+import_data(Filename) ->
+    gen_server:call(?MODULE, {import, Filename}).
+
+handle_call({import, Filename}, _From, State) ->
+    io:format("Importing data from filename ~p ~n", [Filename]),
+    {reply, ok, State};
+
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
---
+++ src/traderdb_app.erl
@@ -0,0 +1,16 @@
+-module(traderdb_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    traderdb_sup:start_link().
+
+stop(_State) ->
+    ok.
